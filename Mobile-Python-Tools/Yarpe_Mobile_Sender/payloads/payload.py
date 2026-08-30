a5e34b69612e57f36235c8ec17347ceb34007bd1
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-08-30 12:43:52 +0300

    Create payload.py

---
+++ Mobile-Python-Tools/Yarpe_Mobile_Sender/payloads/payload.py
@@ -0,0 +1,12 @@
+from sc import sc
+from utils.rp import log
+"""
+btw you can replace this file with whatever file or script you want
+this is a dummy script just for testing and example usage.
+"""
+
+# Send a PlayStation system notification
+sc.send_notification("Hello World from yarpe!")
+
+# Log to the Ren'Py screen
+log("[*] Hello World payload executed successfully.")
